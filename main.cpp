13df7782942f9ab0e5ea16954d967270b9c5ae21
Mani <manihosseini1384@gmail.com>
2024-05-04 09:43:14 +0330

    included some libs

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <sstream>
+#include <utility>
+#include <iomanip>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
