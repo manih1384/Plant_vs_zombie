99d6c561b6951a99bd6e0ccafb2848124e454e91
Mani <manihosseini1384@gmail.com>
2024-05-05 18:37:29 +0330

    global added

---
+++ global.hpp
@@ -0,0 +1,46 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <sstream>
+#include <utility>
+#include <iomanip>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+using namespace sf;
+using namespace std;
+
+
+
+
+
+const int WIDTH = 1400;
+const int HEIGHT = 600;
+const int DX= 80;
+const int DY = 98;
+const int X0 = 294;
+const int Y0 = 130;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
