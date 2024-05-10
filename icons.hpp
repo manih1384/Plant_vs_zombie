620e678bf9bbfd10c2d25ffa81a132ba0617276b
maleki-shayan <malekishayan86@gmail.com>
2024-05-10 23:25:06 +0330

    set icon class

--- icons.hpp
+++ icons.hpp
@@ -2,3 +2,22 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <vector>
+class Icons
+{
+public:
+    Icons();
+    sf::FloatRect get_rect();
+
+private:
+    sf::Texture peashooterIcon;
+    sf::Sprite SpritePeashootericon;
+
+    sf::Texture sunflowerIcon;
+    sf::Sprite SpriteSunflowerIcon;
+
+    sf::Texture wallnutIcon;
+    sf::Sprite SpritewallnutIcon;
+
+    sf::Texture peashooterIcon;
+    sf::Sprite Spriteer;
+};
